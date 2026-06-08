9435b28535880f2c118c0910be68139426f6bd82
Ethan Marks <87040432+ethmarks@users.noreply.github.com>
2026-06-07 22:42:09 -0400

    merge plugins.ts into _config.ts and rename uploads/ to assets/

--- _config.ts
+++ _config.ts
@@ -1,13 +1,23 @@
 import lume from "lume/mod.ts";
-import plugins from "./plugins.ts";
 
-const site = lume({
-  src: "./src",
-});
+// Plugin Imports
+import sass from "lume/plugins/sass.ts";
+import basePath from "lume/plugins/base_path.ts";
+import metas from "lume/plugins/metas.ts";
+import sitemap from "lume/plugins/sitemap.ts";
+import favicon from "lume/plugins/favicon.ts";
 
-site.use(plugins());
+const site = lume({ src: "./src" });
 
-site.add("uploads", "");
-site.add("style.css");
+// Adds
+site.add("assets/style.scss");
+site.add("assets/fonts");
+
+// Plugins
+site.use(sass());
+site.use(basePath());
+site.use(metas());
+site.use(sitemap());
+site.use(favicon({ input: "assets/favicon.svg" }));
 
 export default site;
