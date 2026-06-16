62e10af103ea05120b5777f5d37a98025cf38c05
Ethan Marks <87040432+ethmarks@users.noreply.github.com>
2026-06-16 14:11:05 -0400

    select remote files in mod.ts

--- mod.ts
+++ mod.ts
@@ -1,15 +1,27 @@
-import plugins, { Options } from "./plugins.ts";
+import plugins, { type Options } from "./plugins.ts";
 
 import "lume/types.ts";
-
-export type { Options } from "./plugins.ts";
 
 export default function (options: Partial<Options> = {}) {
   return (site: Lume.Site) => {
     // Configure the site
     site.use(plugins(options));
 
-    // Add the files needed for the theme
-    site.remote("/", import.meta.resolve("./src"), ["/**/*"]);
+    // Add the remote files.
+    //
+    // Important note: these are NOT the files that are to be automatically
+    // copied into the user's site dir on initialization. These files will
+    // _always_ be remotely present as long as the theme is enabled and cannot
+    // be removed (though they can be overridden).
+
+    const dirs = ["assets", "_components", "_includes"];
+    dirs.forEach((dir) => {
+      site.remote(`/${dir}`, import.meta.resolve(`./src/${dir}`), ["/**/*"]);
+    });
+
+    const files = ["style.scss", "_data.yml", "blog/_data.yml"];
+    files.forEach((file) => {
+      site.remote(file, import.meta.resolve(`./src/${file}`));
+    });
   };
 }
